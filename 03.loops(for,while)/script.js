87824760c4aeb882c2d09d5ab2105d1642f8fb9f
Fidan <126713798+FidanAsadova@users.noreply.github.com>
2023-04-07 02:43:09 -0700

    Add files via upload

---
+++ 03.loops(for,while)/script.js
@@ -0,0 +1,36 @@
+// console.log("hello")
+
+// for (let i = 0; i<=10; i++){
+//    console.log(`${i} x ${i} = ${i*i} ${i**3}`)
+// }
+
+
+// classwork
+
+
+
+// //1
+// let myName = "Fidan";
+// for(let i = 0; i < myName.length; i++){
+//     console.log(myName[i])
+// }
+
+// //2
+
+// for(let i = 0; i < 8; i++){
+//     if(i%2==0)
+//     console.log(i)
+// }
+
+// //3
+// for(let index = 0; index < 7; index++){
+//     if(index%2==0){
+//     console.log("even", index);
+//     }
+//     else{
+//         console.log("odd", index);
+//     }
+// }
+
+
+// next
