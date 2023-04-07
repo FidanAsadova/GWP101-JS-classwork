24effb3c215396fb2c5a2465ce4ad96161d8f55d
Fidan <126713798+FidanAsadova@users.noreply.github.com>
2023-04-07 02:07:03 -0700

    Add files via upload

---
+++ 02.if-elseVsSwitch/script.js
@@ -0,0 +1,27 @@
+// console.log("$"+4+5)
+// console.log("4"-2)
+// console.log("-9"-5)
+// console.log(null +1)
+// console.log(undefined +1)
+// console.log("\t \n"/2)
+
+// practice
+
+// let text = "there";
+// console.log(`hello ${3+8}`);
+// console.log(`hello ${`3+8`}`);
+// console.log(`hello ${"text"}`);
+// console.log(`hello ${text}`);
+
+// next 
+
+// let firstName = "Fidan";
+// let lastname = "Asadova"; 
+// let age = 20; 
+// let city = "Goychay"; 
+// let uni = "GDU";
+// let info = `${firstName} ${lastname} ${age} ${city} ${uni}`
+// console.log(info)
+
+let a = 0;
+console.log(a ?? 100)
