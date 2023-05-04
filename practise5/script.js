c54ec4c85465b7ea074084ca215bcd9bafc243a3
Fidan <126713798+FidanAsadova@users.noreply.github.com>
2023-05-04 03:10:28 -0700

    Add files via upload

---
+++ practise5/script.js
@@ -0,0 +1,70 @@
+let todoInput = document.querySelector("#todo-input");
+let addTodo = document.querySelector(".btn-primary");
+let todoList = document.querySelector(".list-group");
+
+addTodo.disabled = true;
+
+let editList = JSON.parse(localStorage.getItem("editList")) || [];
+
+todoInput.addEventListener("input", function (e) {
+  if (e.target.value) {
+    addTodo.disabled = false;
+  } else {
+    addTodo.disabled = true;
+  }
+});
+
+addTodo.addEventListener("click", function (e) {
+  if (todoInput.value) {
+    let listObj = {
+      id: Date.now(),
+      textTodo: todoInput.value,
+    };
+    editList.push(listObj);
+    localStorage.setItem("todos", JSON.stringify(editList));
+  }
+
+  list();
+  todoInput.value = "";
+});
+
+function list() {
+  todoList.innerHTML = "";
+  editList.forEach((item) => {
+    todoList.innerHTML += `
+          <li
+                class="list-group-item list-group-item-info my-2 d-flex justify-content-between align-items-center rounded"
+              >
+                <span>${item.textTodo}</span>
+                <div>
+                  <button type="button" class="btn btn-success onclick=editTodo(${item.id})">
+                    Edit
+                  </button>
+                  <button type="button" class="btn btn-danger deleteBtn" onclick=removeTodo(${item.id})>
+                    Delete
+                  </button>
+                </div>
+              </li>`;
+  });
+  todoInput.value = "";
+}
+
+function removeTodo(id) {
+  editList = editList.filter((el) => el.id != id);
+  localStorage.setItem("todos", JSON.stringify(editList));
+  list();
+}
+
+let editBtn = document.querySelector(".btn-success");
+
+editBtn.forEach((item) => {
+    item.addEventListener("click", function () {
+      todoInput.value = this.closest("li").firstElementChild.innerText;
+      todoInput.focus();
+      this.closest("li").remove();
+    });
+  });
+
+  todoInput.value = "";
+  addTodo.disabled = true;
+
