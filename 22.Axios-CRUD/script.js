06c63eba625b842493fd33408a59e3e5f7ecde57
FidanAsadova <7ksn2xn@code.edu.az>
2023-05-14 00:10:53 +0400

    change added

--- 22.Axios-CRUD/script.js
+++ 22.Axios-CRUD/script.js
@@ -4,17 +4,23 @@ const search = document.querySelector("#searchInput");
 const sort = document.querySelector("#thFirstName");
 const arrowUp = document.querySelector(".arrowUp");
 const arrowDown = document.querySelector(".arrowDown");
+const submitBtn = document.querySelector("#submitBtn");
 let allInputs = document.querySelectorAll(".form-control");
 let notification = document.querySelectorAll(".notification");
+let date = new Date();
+let isEdit = false;
 let bool = false;
-// let array = [];
-// async function getData() {
-//   const res = await axios.get("http://localhost:8080/users");
-//   const data = res.data;
-//   console.log(data);
-// array = data;
-//   drawTable(data);
-// }
+
+
+
+function showAlert(message, className) {
+  notification.innerHTML = message
+  notification.className = `alert alert-${className}`
+  notification.removeAttribute('hidden')
+  setTimeout(() => {
+      notification.setAttribute('hidden', '')
+  }, 2000)
+}
 
 axios("http://localhost:8080/users").then((res) => {
   drawTable(res.data);
@@ -22,25 +28,41 @@ axios("http://localhost:8080/users").then((res) => {
 
 function drawTable(array) {
   tBody.innerHTML = "";
-
-  //   const res = await axios.get("http://localhost:8080/users");
-  //   const data = res.data;
-  //   console.log(data);
-  //   array = data;
   array.forEach((element) => {
     tBody.innerHTML += `
     <tr>
     <td>${element.firstname}</td>
     <td>${element.lastname}</td>
     <td>${element.email}</td>
-    <td>    <a class= "form-control btn btn-warning w-25" onclick=editData("${element.id}")>Edit</a>
-    
-    <a class= "form-control btn btn-danger w-50" onclick=deleteData("${element.id}")>Delete</a></td>
+    <td>${element.password.split``.fill("*", 0, element.password.length - 4)
+      .join``}</td>
+    <td><a class= "form-control btn btn-warning w-25" onclick=editData("${
+      element.id
+    }")>Edit</a>
+    <a class= "form-control btn btn-danger w-25" onclick=deleteData("${
+      element.id
+    }")>Delete</a>
+    <a class= "form-control btn btn-primary w-25" onclick=getUserDetail('${element.id}','${element.name}','${element.date}')>Detail</a></td>
     </tr>
     `;
   });
 }
 // drawTable();
+function createdData() {
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    let obj = {
+      firstname: allInputs[0].value,
+      lastname: allInputs[1].value,
+      email: allInputs[2].value,
+      password: allInputs[3].value,
+      date: date.toDateString(),
+    };
+    await axios.post(`http://localhost:8080/users/`, obj);
+    showAlert('Added users successfully!', 'success')
+  });
+}
+createdData();
 
 search.addEventListener("input", function (event) {
   axios("http://localhost:8080/users").then((res) => {
@@ -82,7 +104,7 @@ sort.addEventListener("click", function () {
 
 async function deleteData(id) {
   await axios.delete(`http://localhost:8080/users/${id}`);
-  notification.style.display = "inline-block";
+  showAlert(`User:${username} has been delete on`, `danger`);
 }
 
 async function editData(id) {
@@ -90,15 +112,23 @@ async function editData(id) {
     allInputs[0].value = res.data.firstname;
     allInputs[1].value = res.data.lastname;
     allInputs[2].value = res.data.email;
-
+    allInputs[3].value = res.data.password;
     form.addEventListener("submit", async function (e) {
       e.preventDefault();
       let obj = {
         firstname: allInputs[0].value,
         lastname: allInputs[1].value,
         email: allInputs[2].value,
+        password: allInputs[3].value,
       };
       await axios.patch(`http://localhost:8080/users/${id}`, obj);
+      showAlert('Edited User successfully!', 'success')
     });
   });
 }
+
+// async function getUserDetail(id, username, userDate) {
+//   await axios(`http://localhost:8080/users/${id}`);
+//   showAlert(`User: ${username} has been created on - ${userDate}!`, "primary");
+//   // console.log(username);
+// }
